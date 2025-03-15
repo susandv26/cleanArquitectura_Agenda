2e459b8b21590f18b7e78ca341867da1988058cf
susandv26 <valladares.susan@yahoo.com>
2025-03-15 16:45:19 -0600

    feacture: arquitectura inicial

---
+++ backend/API/Configuration/DatabaseConfig.cs
@@ -0,0 +1,30 @@
+﻿
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
+
+
+namespace API.Configuration
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; }
+
+        public DatabaseConfig()
+        {
+            DotNetEnv.Env.Load();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            string dbServer = configuration["DB_SERVER"] ?? "localhost";
+            string dbPort = configuration["DB_PORT"] ?? "1433";
+            string dbName = configuration["DB_NAME"] ?? "Bd_Agenda";
+            string dbUser= configuration["DB_USER"] ?? "sa";
+            string dbPassword = configuration["DB_PASSWORD"] ?? "123";
+
+            ConnectionString = $"Server={dbServer},{dbPort}; Database = {dbName};User Id ={dbUser};Password = {dbPassword}; TrustServerCertificate=True";
+        }
+    }
+}
---
+++ backend/Dominio/Entities/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Common;
+
+namespace Dominio.Entities
+{
+    public class Usuario : Entity
+    {
+        public string Nombre { get; set; }
+        public string Correoelectronico { get; set; }
+        public string Password { get; set; }
+
+
+        public Usuario(string nombre, string correoelectronico, string password)
+        {
+            Nombre = nombre;
+            Correoelectronico = correoelectronico;
+            Password = password;
+        }
+    }
+}
