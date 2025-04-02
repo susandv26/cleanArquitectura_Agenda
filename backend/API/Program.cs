91612953dc476c9f299b7985dff5342fa80b9d0a
PSamu23 <samuelespinal90@gmail.com>
2025-04-01 18:25:30 -0600

    se agregó el docker file

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -5,9 +5,8 @@ using Infraestructure.Persistence;
 using Infraestructure.Repositorio.Entidades;
 using Microsoft.EntityFrameworkCore;
 using Application.Interfaces.Entidades;
-using Application.Interfaces.Actividades;
-using Infraestructure.Repositorio.Actividades;
-using Application.Services.Actividades;
+using Infraestructure;
+
 
 
 namespace API
@@ -17,10 +16,6 @@ namespace API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-
-
-
 
 
             // Add services to the container.
@@ -37,25 +32,9 @@ namespace API
             builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             builder.Services.AddScoped<IUsuarioService, UsuarioService>();
             //liena extra 3.2
-            builder.Services.AddScoped<ITipoRepository, TipoRepository>();
-            builder.Services.AddScoped<ITipoService, TipoService>();
 
-           // builder.Services.AddScoped<ITipoRepository, TipoRepository>();
-           // builder.Services.AddScoped<ITipoService, TipoService>();
+           builder.Services.AddInfrastructure();
 
-           // builder.Services.AddScoped<ITareaRepository, TareaRepository>();
-           // builder.Services.AddScoped<ITareaService, TareaService>();
-
-            builder.Services.AddScoped<IActividadRepository, ActividadRepository>();
-            builder.Services.AddScoped<IActividadService, ActividadService>();
-
-            
-
-
-
-
-
-            //liena extra 4
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -72,27 +51,10 @@ namespace API
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+            app.MapControllers();
 
             app.Run();
+
         }
     }
 }
