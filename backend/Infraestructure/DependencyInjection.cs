afc1ddab0a37dd00480fec7b0352401ab29a00da
LeviHernandez315 <levi.hernandez@unah.hn>
2025-04-03 00:20:29 -0600

    fix: eliminacion de usuario viejo

--- backend/Infraestructure/DependencyInjection.cs
+++ backend/Infraestructure/DependencyInjection.cs
@@ -1,13 +1,4 @@
-﻿using Application.DTOs;
-using Application.Interfaces;
-using Application.Services;
-using Dominio.Entities;
-using Infraestructure.Repositorio;
-using Microsoft.Extensions.DependencyInjection;
-using Application.Interfaces.Entidades;
-using Application.Services.Entidades;
-using Infraestructure.Repositorio.Entidades;
-
+﻿using Microsoft.Extensions.DependencyInjection;
 namespace Infraestructure
 {
 
@@ -16,8 +7,6 @@ namespace Infraestructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
            
-            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-            services.AddScoped<IUsuarioService, UsuarioService>();
 
             return services;
         }
