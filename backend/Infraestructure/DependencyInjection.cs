0d288f90ee6cd8427c280c388b718a0ed9abca8b
susandv26 <valladares.susan@yahoo.com>
2025-04-04 19:51:29 -0600

    modificacion en programs.cs y backendContext.cs

--- backend/Infraestructure/DependencyInjection.cs
+++ backend/Infraestructure/DependencyInjection.cs
@@ -1,4 +1,11 @@
 ﻿using Application.Interfaces.Entidades.AreaTrabajoInterfaces;
+using Application.Interfaces.Entidades.CiudadInterfaces;
+using Application.Interfaces.Entidades.DepartamentoInterfaces;
+using Application.Interfaces.Entidades.DireccionInterfaces;
+using Application.Interfaces.Entidades.PaisInterfaces;
+using Application.Interfaces.Entidades.PersonaInterfaces;
+using Application.Interfaces.Entidades.PuestoTrabajoInterfaces;
+using Application.Interfaces.Entidades.RolInterfaces;
 using Application.Services.Entidades;
 using Infraestructure.Repositorio.Entidades;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +22,26 @@ namespace Infraestructure
             services.AddScoped<IAreaTrabajoService, AreaTrabajoService>();
 
             //Aquí agregar las otras entidades y agregados
+            services.AddScoped<IPaisRepository, PaisRepository>();
+            services.AddScoped<IPaisService, PaisService>();
+
+            services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
+            services.AddScoped<IDepartamentoService, DepartamentoService>();
+
+            services.AddScoped<ICiudadRepository, CiudadRepository>();
+            services.AddScoped<ICiudadService, CiudadService>();
+
+            services.AddScoped<IDireccionRepository, DireccionRepository>();
+            services.AddScoped<IDireccionService, DireccionService>();
+
+            services.AddScoped<IPersonaRepository, PersonaRepository>();
+            services.AddScoped<IPersonaService, PersonaService>();
+
+            services.AddScoped<IRolRepository, RolRepository>();
+            services.AddScoped<IRolService, RolService>();
+
+            services.AddScoped<IPuestoTrabajoRepository, PuestoTrabajoRepository>();
+            services.AddScoped<IPuestoTrabajoService, PuestoTrabajoService>();
 
             // Registrar JwtService como Singleton
             services.AddSingleton<JwtService>();
